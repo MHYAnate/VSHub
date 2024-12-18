cbe5090ba2649c4059cc179c05243972b09c7caa
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2024-12-18 16:35:57 +0100

    YA ALLAH'UL HAMID

--- src/components/dashboard/upDateLocation.tsx
+++ src/components/dashboard/upDateLocation.tsx
@@ -113,7 +113,7 @@ export default function UpDateLocation(
 				</label>
 				<select
 					value={
-						selectState !== (undefined || null)
+						 selectState !== undefined && selectState !== null
 							? selectState
 							: selectCountry === "Nigeria"
 							? ""
