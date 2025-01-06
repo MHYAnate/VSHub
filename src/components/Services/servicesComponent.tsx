02b35e2ae5b6da47e1828f862bffeeda90cdfcb7
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-01-06 16:46:09 +0100

    serviceCard@mobile

--- src/components/Services/servicesComponent.tsx
+++ src/components/Services/servicesComponent.tsx
@@ -39,16 +39,16 @@ const ServicesComponent: React.FC = () => {
 						}}
 						className="bg-white rounded-lg overflow-hidden transition-all duration-300 hover:shadow-2xl transform hover:-translate-y-2"
 					>
-						<div className="relative overflow-hidden group">
+						<div className="relative w-full h-48 mb-4 rounded-lg overflow-hidden">
 							<Image
 								src={category.src}
 								alt={`${category.category} category`}
 								width={400}
 								height={200}
-								className="w-full h-48 object-cover transition-transform duration-500 group-hover:scale-110"
+								className="transition-transform duration-300 hover:scale-110"
 							/>
 							<div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-								<h3 className="text-2xl font-semibold text-white">
+								<h3 className="text-lg font-semibold text-gray-900 ">
 									{category.category}
 								</h3>
 							</div>
