cc8c724b6e1a5dc6d9d351a68c1e849c57c3393d
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-01-06 12:40:58 +0100

    fix:png

--- src/database/carouselData.ts
+++ src/database/carouselData.ts
@@ -46,7 +46,7 @@ export const Cards = [
 		type: "benefit",
 		title: "24/7 Support",
 		description: "Get help anytime with our round-the-clock customer service.",
-		image: "/service/512.jpg",
+		image: "/service/512.png",
 		cta: "Contact Us",
 	},
 ];
