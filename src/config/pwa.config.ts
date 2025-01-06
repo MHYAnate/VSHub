3850d1b9dacb0ff8fc8f3a5a58955c537ca1d0d4
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-01-06 12:25:33 +0100

    fix:appIcon

--- src/config/pwa.config.ts
+++ src/config/pwa.config.ts
@@ -10,13 +10,13 @@ export const pwaConfig = {
   startUrl: '/',
   icons: [
     {
-      src: '/service/111.png',
+      src: '/service/192.png',
       sizes: '192x192',
       type: 'image/png',
       purpose: 'any maskable'
     },
     {
-      src: '/service/111.png',
+      src: '/service/512.png',
       sizes: '512x512',
       type: 'image/png'
     }
