977e28708dbe93d6827130d04ce496eaf0bbca5c
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-01-13 13:37:49 +0100

    text-center:serviceCard

--- src/components/Services/servicesBComponent.tsx
+++ src/components/Services/servicesBComponent.tsx
@@ -50,7 +50,7 @@ const ServicesBComponent: React.FC = () => {
                 className="w-full h-48 object-cover transition-transform duration-500 group-hover:scale-110"
               />
               <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <h3 className="text-2xl font-semibold text-white">
+                <h3 className="text-2xl font-semibold text-center text-white">
                   {category.category}
                 </h3>
               </div>
